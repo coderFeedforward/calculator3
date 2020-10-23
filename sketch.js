b9565da10e7f29652a430dd0d34eb5b97438c2ca
coderFeedforward <68401751+coderFeedforward@users.noreply.github.com>
2020-10-23 15:56:08 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var count = 0;
+var change = true;
+var a = 0;
+var pic;
+//function preload(){
+  //pic = loadImage("hand.jpg");
+//}
+
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+
+  
+}
+
+function draw() {
+  angleMode(DEGREES)
+  /*
+  translate(200,200);
+  image(pic, 0,0,200,200);
+  rotate(-50);
+  */
+  
+  
+  translate(200,200);
+
+  //imageMode(CENTER);
+  rectMode(CENTER);
+
+  rotate(30);
+  if(count > 100){
+    
+    a += 1;
+    rotate(a);
+  }else{
+    a += -1;
+    rotate(a);
+  }
+  
+  
+
+  
+  background(220);
+  //image(pic, 0,0,200,200);
+  rect(0,0,200,200)
+
+  
+  count = count + 1;
+  print(count);
+  
+  if(count % 205 == 0){
+    count = 0;
+  }
+  
+  
+  
+}
+
